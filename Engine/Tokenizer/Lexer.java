c634af7bc40fafbd9e9503a2cf613fb602d30078
vestex727 <smastrok9495@gmail.com>
2026-03-08 03:42:58 -0400

    Added full support for C and Python. Fixed a bug that broke the all tokens before a keyword token if said keyword was not the first keyword in the list. Tokenizer should now be fully completed until fault testing.

--- Engine/Tokenizer/Lexer.java
+++ Engine/Tokenizer/Lexer.java
@@ -3,22 +3,26 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/** Takes in code in the form of a String, outputs a list of Tokens that catagorize and order each piece of code.
+/** Takes in code in the form of a String, outputs a list of Tokens that categorizes and orders each piece of code.
  *  These tokens will be used for comparisons for plagiarism detection.
  *  These tokens will be used to reconstruct the report later.
  *
- *  TODO add Python and C support
  *
  *  Based on Enzo Jade's Tokenizer from the linked tutorial: https://medium.com/@enzojade62/step-by-step-building-a-lexer-in-java-for-tokenizing-source-code-ac4f1d91326f
- *  Modified to support comments, Strings, and non-Java languages.
+ *  Modified to support comments, Strings, and non-Java languages
+ *  Modified to make Keywords not break the lexer if not the first keyword in a list
  *
- * @Version 1.1.0 (Feb 11th, 2026)
+ * @Version 1.2.0 (March 8th, 2026)
  */
 public class Lexer {
     private String input;
     private final Language language;
     private int index;
     FiniteStateMachine currentState;
+    private static final String JAVA_KEYWORDS = "_|abstract|assert|boolean|break|byte|case|catch|char|class|continue|default|do(uble)?|else|enum|extends|final(ly)?|float|for|if|implements|import|instanceof|int(erface)?|long|native|new|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while|const|goto|strictfp";
+    private static final String C_KEYWORDS = "auto|break|case|char|const|continue|default|do(uble)?|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|inline|restrict|_(Bool|Complex|Imaginary_Alignas|Alignof|Atomic|Generic|NoReturn|Static_assert|Thread_local|BitInt|Decimal(32|64|128))|align(as|of)|bool|constexpr|false|nullptr|static_assert|thread_local|true|typeof(_unequal)?";
+    private static final String PYTHON_KEYWORDS = "if|for|while|try|raise|class|def|with|break|continue|del|pass|assert|yield|return|import|from|match|case|bool|byte(array|s)|complex|dict|types|EllipsisType|float|frozenset|int|list|NoneType|NotImplementedType|range|set|str|tuple";
+
 
     public Lexer(String input){
         this.input = input;
@@ -26,9 +30,10 @@ public class Lexer {
         this.index = 0;
     }
 
-    /** Creates a Lexer (also known as a tokenizer,) that can be used to tokenize the given String input
+    /** Creates a Lexer (also known as a tokenizer) that can be used to tokenize the given String input
      *
      * @param input Code to be tokenized
+     * @param language Language of the code
      */
     public Lexer(String input, Language language){
         this.input = input;
@@ -37,7 +42,6 @@ public class Lexer {
     }
 
     /** Creates the list of tokens from the given text from construction
-     *  TODO I'm pretty sure a double whitespace breaks it. That needs to be fixed at some point.
      *
      * @return  List of Tokens
      */
@@ -67,33 +71,42 @@ public class Lexer {
         correctStrings(tokens);
         correctCharacters(tokens);
         condenseStringTokens(tokens);
+        correctKeywords(tokens);
         return tokens;
     }
 
     /** Creates the next Token beginning at the current index
      *  Compares the current string to the regular expressions representing the different types of Tokens
+     *  Does not identify strings, chars or keywords. Those must be done independently, later with the correctKeywords,
+     *  correctCharacters, and correctKeywords functions
      *  TODO Need to make this more resilient. Currently handles errors poorly
      *
-     * @return  Null if something fails,
+     * @return  null if something fails, token made from next token of code otherwise
      */
     private Token nextToken() {
         if (index >= input.length()) {
             return null;
         }
 
+        String keywords;
+        switch (language){
+            case Java -> keywords = JAVA_KEYWORDS;
+            case C -> keywords = C_KEYWORDS;
+            case Python -> keywords = PYTHON_KEYWORDS;
+            default -> keywords = "";
+        }
+
         String[] tokenPatterns = {
-                "_|abstract|assert|boolean|break|byte|case|catch|char|class|continue|default|do|double|else|enum|extends|final|finally|float|for|if|implements|import|instanceof|int|interface|long|native|new|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while|const|goto|strictfp",         // Keywords
                 "[a-zA-Z_][a-zA-Z0-9_]*",   // Identifiers
-                "\\d+",              // Literals
-                "//[a-zA-Z0-9_]*",          // Single-Line Commments
+                "\\d+",                     // Literals
+                "//[a-zA-Z0-9_]*",          // Single-Line Comments
                 "/\\*[a-zA-Z0-9_]*",        // Multi-Line Comments
                 "\\*/",                     // Comment End
-                "[+-/*=<>!?]",              // Operators
-                "[.,;(){}\"']",              // Punctuation
+                "[+/*=<>!?&|^~:%@-]",       // Operators
+                "[.,;(){}\"']",             // Punctuation
         };
 
         TokenType[] tokenTypes = {
-                TokenType.KEYWORD,
                 TokenType.IDENTIFIER,
                 TokenType.LITERAL,
                 TokenType.SINGLE_LINE_COMMENT,
@@ -106,18 +119,20 @@ public class Lexer {
         for (int i = 0; i < tokenPatterns.length; i++) {
             Pattern pattern = Pattern.compile("^" + tokenPatterns[i]);
             Matcher matcher = pattern.matcher(input.substring(index));
-
             if (matcher.find()) {
                 String value = matcher.group();
                 index += value.length();
                 TokenType t = tokenTypes[i];
 
-
+                /** Removed, as comments are now handled earlier in the process
+                 * Commented out in case needed in the future
+                 *
                 if(t == TokenType.SINGLE_LINE_COMMENT) currentState = FiniteStateMachine.SINGLE_LINE_COMMENT;
                 if(t == TokenType.MULTI_LINE_COMMENT) currentState = FiniteStateMachine.MULTI_LINE_COMMENT;
                 if(t == TokenType.COMMENT_END) currentState = FiniteStateMachine.CODE;
                 if(currentState == FiniteStateMachine.SINGLE_LINE_COMMENT || currentState == FiniteStateMachine.MULTI_LINE_COMMENT)  return new Token(TokenType.COMMENT, value);
-                System.out.println(value);
+                */
+
                 return new Token(tokenTypes[i], value);
             }
         }
@@ -178,7 +193,7 @@ public class Lexer {
     }
 
     /**Rewrites the type of any tokens between single quote tokens as literals, as they are character values.
-     * Will only update if a single token is nestled between the quotes
+     * Will only update if a single token is nestled between the quotes.
      *
      * @param tokens    List of tokens to modify
      * @return  updated List
@@ -202,6 +217,30 @@ public class Lexer {
             }
 
             counter++;
+        }
+        return tokens;
+    }
+
+    /**Rewrites the type of any tokens that fit within the requirements to be a keyword token.
+     * The token must not be part of a string token, and must fit within the language appropriate keyword regex.
+     *
+     * @param tokens    List of tokens to modify
+     * @return  updated List
+     */
+    private List<Token> correctKeywords(List<Token> tokens){
+        String keywords;
+        switch (language){
+            case Java -> keywords = JAVA_KEYWORDS;
+            case C -> keywords = C_KEYWORDS;
+            case Python -> keywords = PYTHON_KEYWORDS;
+            default -> keywords = "";
+        }
+
+        Pattern pattern = Pattern.compile("^" + keywords);
+
+
+        for(Token token : tokens){
+            if(token.getType() == TokenType.IDENTIFIER && token.getValue().matches(keywords)) token.updateType(TokenType.KEYWORD);
         }
         return tokens;
     }
--- Engine/Tokenizer/Tester.java
+++ Engine/Tokenizer/Tester.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class Tester {
     public static void main(String[] args) {
         String code3 = "if (x > 10) { y = x-5; } //test \n/*test 1 2 3 4 5 6 7 8 9*/ 10 11 12 13 14 15";
-        String code = "test (x > 10) { y = x - 5; } //test\n\"a b c d\"";
+        String code = "test (x > 10) {_ y = x - 5; } for //test\n _ _  _ \"a b c d\"";
         String code2 = "for (i = 0; i < 10; i++){\nx +=1;\nSystem.out.printLn(x);}";
+        String code4 = "test (x > 10) {_ y = x - NULL; } ";
 
         Lexer lexer = new Lexer(code);
         List<Token> tokens = lexer.tokenize();
