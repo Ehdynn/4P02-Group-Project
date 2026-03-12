da808c3d135492bb91fd728d34216022dc715759
Kevin121602 <kevin.james.gracey@gmail.com>
2026-03-12 12:02:19 -0400

    Adjusted tiling function to take in a list of token sets as its second parameter

--- Engine/Tokenizer/StringTiling.java
+++ Engine/Tokenizer/StringTiling.java
@@ -3,85 +3,85 @@ import java.util.List;
 
 public class StringTiling {
 
-    /** Takes in two different lists of tokens, outputs a list containing sequences of identical tokens greater than a certain length.
-     *
-     *  TODO make compatible with list of submissions rather than single list of tokens
+    /** Takes in an array of tokens and a list of token arrays, outputs a list containing sequences of identical tokens greater than a certain length.
      *
      *  Based on the Greedy-String-Tiling algorithm described on Louis Tarvin's linked website: https://louistarvin.uk/projects/plagiarism/
      *  Modified to only return the similar sequences of the first submission being compared.
      *
      * @Version 1.2 (Mar 12th, 2026)
      */
-    public static List<Sequence> tile(Token[] A, Token[] B, int minLength){
+    public static List<Sequence> tile(Token[] submission, List<Token[]> database, int minLength){
         List<Sequence> matches = new ArrayList<>();
-        int numMatches = 1;
-        boolean[] marked = new boolean[A.length];
+        boolean[] marked = new boolean[submission.length];
 
-        // Iterative loop which finds the largest remaining common sequences
-        while(numMatches != 0){
-            List<Sequence> subMatches = new ArrayList<>();
-            //array for keeping track of sequence starts, so two sequences of the same start and length aren't reused
-            List<Integer> matchStarts = new ArrayList<>();
-            List<Token> tempTokens = new ArrayList<>();
-            int lcs = minLength;
+        for(Token[] compareTo: database){
+            int numMatches = 1;
+            // Iterative loop which finds the largest remaining common sequences
+            while(numMatches != 0){
+                List<Sequence> subMatches = new ArrayList<>();
+                //array for keeping track of sequence starts, so two sequences of the same start and length aren't reused
+                List<Integer> matchStarts = new ArrayList<>();
+                List<Token> tempTokens = new ArrayList<>();
+                int lcs = minLength;
 
-            for (int i = 0; i < A.length; i++) {
-                if (marked[i]) continue;
+                for (int i = 0; i < submission.length; i++) {
+                    if (marked[i]) continue;
 
-                for (int j = 0; j < B.length; j++) {
+                    for (int j = 0; j < compareTo.length; j++) {
 
-                    int n = 0;
+                        int n = 0;
 
-                    // Finds n next common tokens
-                    while (i + n < A.length &&
-                            j + n < B.length &&
-                            !marked[i + n] &&
-                            A[i + n].compareTo(B[j + n]) >= 0) {
-                        n++;
-                        if(A[i + n].compareTo(B[j + n]) == 1){
-                            //if token has identical type and value, assign full plagiarism value
-                            A[i + n].setPlagiarismValue(1);
-                        } else if (A[i + n].compareTo(B[j + n]) == 0){
-                            //if token has identical type but different value, assign partial plagiarism value
-                            A[i + n].setPlagiarismValue(0.9);
+                        // Finds n next common tokens
+                        while (i + n < submission.length &&
+                                j + n < compareTo.length &&
+                                !marked[i + n] &&
+                                submission[i + n].compareTo(compareTo[j + n]) >= 0) {
+                            n++;
+                            if(submission[i + n].compareTo(compareTo[j + n]) == 1){
+                                //if token has identical type and value, assign full plagiarism value
+                                submission[i + n].setPlagiarismValue(1);
+                            } else if (submission[i + n].compareTo(compareTo[j + n]) == 0){
+                                //if token has identical type but different value, assign partial plagiarism value
+                                submission[i + n].setPlagiarismValue(0.9);
+                            }
+                            tempTokens.add(submission[i + n]);
                         }
-                        tempTokens.add(A[i + n]);
+
+                        // Adds largest sequence(s) to matches list
+                        if(n > lcs){
+                            subMatches.clear();
+                            matchStarts.clear();
+                            lcs = n;
+                            Sequence newSequence = new Sequence(i, n);
+                            for(Token t: tempTokens){
+                                newSequence.addToken(t);
+                            }
+                            subMatches.add(newSequence);
+                            matchStarts.add(i);
+                        } else if(n == lcs && !matchStarts.contains(i)){
+                            Sequence newSequence = new Sequence(i, n);
+                            for(Token t: tempTokens){
+                                newSequence.addToken(t);
+                            }
+                            subMatches.add(newSequence);
+                            matchStarts.add(i);
+                        }
+
+                        tempTokens.clear();
                     }
+                }
 
-                    // Adds largest sequence(s) to matches list
-                    if(n > lcs){
-                        subMatches.clear();
-                        matchStarts.clear();
-                        lcs = n;
-                        Sequence newSequence = new Sequence(i, n);
-                        for(Token t: tempTokens){
-                            newSequence.addToken(t);
-                        }
-                        subMatches.add(newSequence);
-                        matchStarts.add(i);
-                    } else if(n == lcs && !matchStarts.contains(i)){
-                        Sequence newSequence = new Sequence(i, n);
-                        for(Token t: tempTokens){
-                            newSequence.addToken(t);
-                        }
-                        subMatches.add(newSequence);
-                        matchStarts.add(i);
+                // Marks all matched tokens and adds them to master list
+                for(Sequence s: subMatches){
+                    for(int k = 0; k < s.getLength(); k++){
+                        marked[s.getStart() + k] = true;
                     }
-
-                    tempTokens.clear();
+                    matches.add(s);
                 }
-            }
 
-            // Marks all matched tokens and adds them to master list
-            for(Sequence s: subMatches){
-                for(int k = 0; k < s.getLength(); k++){
-                    marked[s.getStart() + k] = true;
-                }
-                matches.add(s);
+                // Checks number of matches found this iteration, exits loop if 0
+                numMatches = subMatches.size();
             }
-
-            // Checks number of matches found this iteration, exits loop if 0
-            numMatches = subMatches.size();
         }
 
         return matches;
