87186a5ecce513098679785f05901b1891c583f6
Troy Neal <71712119+Troy-Neal@users.noreply.github.com>
2026-03-16 19:55:50 -0400

    Added File References For New Submission Table

---
+++ Database/TableSchema/File_Submissions_New.sql
@@ -0,0 +1,9 @@
+create table public."File_Submissions_New" (
+  id uuid not null default gen_random_uuid (),
+  assignment_id bigint null,
+  created_at timestamp with time zone not null default now(),
+  file_name text not null,
+  student_info jsonb null,
+  constraint File_Submissions_New_pkey primary key (id),
+  constraint File_Submissions_New_assignment_id_fkey foreign KEY (assignment_id) references "Assignments" (id) on update CASCADE on delete CASCADE
+) TABLESPACE pg_default;
