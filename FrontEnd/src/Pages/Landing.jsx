aa939a61e90d526f44a30bce1f6e85a0eb0aa37d
nealt7558 <troyarthurdavid@gmail.com>
2026-03-17 16:17:28 -0400

    Added basic template to landing page

--- FrontEnd/src/Pages/Landing.jsx
+++ FrontEnd/src/Pages/Landing.jsx
@@ -2,7 +2,34 @@ import useUser from "../context/useUser"
 
 const Landing = () => {
   const {isProfessor, roleReady } = useUser();
-  return (<p>{isProfessor ? "Prof": "not prof"}</p>)
+  return (
+    <main className="outer-container-3qw">
+      <h1 className="h1-default text-center">Team Won Code Comparison</h1>
+      <div className="flex w-full space-x-5 flex-col md:flex-row">
+        <div className="flex-1 min-w-0">
+          <div className="box-wrapper">
+            <h2 className="h2-large text-center">Instructors</h2>
+            <div className="box-wrapper-square">
+              <p>
+                Instructor Description
+              </p>
+              </div>
+          </div>
+        </div>
+        <div className="flex-1 min-w-0">
+        <div className="box-wrapper">
+            <h2 className="h2-large text-center">Students</h2>
+            <div className="box-wrapper-square">
+              <p>
+                Student Description
+              </p>
+            </div>
+            <button className="submit-button">Submit An Assignment</button>
+            </div>
+        </div>
+      </div>
+    </main>
+  )
 
 }
 
