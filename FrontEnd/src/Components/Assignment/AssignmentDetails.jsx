1da80f3488009a74c8fc74b6e5f700ab74b65d9c
nealt7558 <troyarthurdavid@gmail.com>
2026-03-19 19:11:20 -0400

    Submission key is shown on the assignment page

--- FrontEnd/src/Components/Assignment/AssignmentDetails.jsx
+++ FrontEnd/src/Components/Assignment/AssignmentDetails.jsx
@@ -1,13 +1,16 @@
 const AssignmentDetails = ({details}) => {
+
   return (
     <>
     <h1 className="h1-default text-center">{details.name ?? "Assignment"}</h1>
     <p className="">Due on {details.due_date ? (new Date(details.due_date).toLocaleString('en-US', {dateStyle: "medium"}) + " " + new Date(details.due_date).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })) : "No Due Date Provided"}</p>
+    <h2 >Submission Code: {details.key}</h2>
     <div className="box-wrapper-square">
         <h2 className="h2-large text-center">Description</h2>
         <hr className="h-px my-8 bg-neutral-quaternary border-sm border-gray-500"/>
         <p>{details.description ?? "No description provided."}</p>
     </div>
+    
     </>
   )
 }
