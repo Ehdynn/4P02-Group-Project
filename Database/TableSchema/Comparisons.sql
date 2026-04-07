e050777e745082eaa44c9b80b70a02ab8f67a740
nealt7558 <troyarthurdavid@gmail.com>
2026-04-07 16:27:54 -0400

    Update Comparisons.sql
    Added a submissions compared column.

--- Database/TableSchema/Comparisons.sql
+++ Database/TableSchema/Comparisons.sql
@@ -8,6 +8,7 @@ create table public."Comparisons" (
   number_of_students integer null,
   boiler_plate_file uuid null,
   repository uuid null,
+  submissions_compared uuid[] null,
   constraint Comparisons_pkey primary key (id),
   constraint Comparisons_aid_fkey foreign KEY (aid) references "Assignments" (id),
   constraint Comparisons_boiler_plate_file_fkey foreign KEY (boiler_plate_file) references "Boiler_Plate_Uploads" (id),
